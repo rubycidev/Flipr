937cef3b963321b51acc4c2d47cdf407ebc7c187
SasaV-Kolosek <sasavas@kolosek.com>
2024-09-20 14:04:30 +0200

    added tests

--- test/controllers/api/sessions_controller_controller_test.rb
+++ test/controllers/api/sessions_controller_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class Api::SessionsControllerControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
 end
